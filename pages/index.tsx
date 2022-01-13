0086ee5802570a84bb0ec8a4ffe96e54f2d6f71c
jinwook-song <wlsdnr129@naver.com>
2022-01-13 20:28:46 +0900

    tailwind setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,11 @@
 import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
-  return null;
+  return (
+    <div className='bg-red-500'>
+      <h1 className='bg-blud-500'>tailwind</h1>
+    </div>
+  );
 };
 
 export default Home;
