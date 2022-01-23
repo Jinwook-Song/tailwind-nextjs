c70fa568409f8259c2f598c456c1ded1209ce896
jinwook song <wlsdnr129@naver.com>
2022-01-23 16:26:29 +0900

    tailwind demo test

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,29 @@ import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
   return (
-    <div className='bg-red-500'>
-      <h1 className='bg-blud-500'>tailwind</h1>
+    <div className='bg-slate-400 py-20 px-8 flex flex-col space-y-5'>
+      <div className='bg-white p-6 rounded-lg shadow-2xl'>
+        <span className='font-semibold text-3xl'>Select Item</span>
+        <div className='flex justify-between my-2'>
+          <span className='text-gray-500'>Grey Chair</span>
+          <span className='font-semibold'>$19</span>
+        </div>
+        <div className='flex justify-between'>
+          <span className='text-gray-500'>Tooly Table</span>
+          <span className='font-semibold'>$8</span>
+        </div>
+        <div className='flex justify-between mt-2 pt-2 border-t-2 border-dashed'>
+          <span>Total</span>
+          <span className='font-semibold'>$27</span>
+        </div>
+        <div className='mt-5 bg-blue-500 text-white py-3 text-center rounded-3xl w-3/4 mx-auto'>
+          Checkout
+        </div>
+      </div>
+      <div className='bg-white p-10 rounded-lg shadow-2xl'></div>
+      <div className='bg-white p-10 rounded-lg shadow-2xl'></div>
+      <div className='bg-white p-10 rounded-lg shadow-2xl'></div>
     </div>
   );
 };
-
 export default Home;
