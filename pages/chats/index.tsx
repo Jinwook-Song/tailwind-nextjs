f60f7689a195b99b7c0d678b5719ffbdac2fff08
jinwook song <wlsdnr129@naver.com>
2022-03-05 19:26:33 +0900

    layout 적용

--- pages/chats/index.tsx
+++ pages/chats/index.tsx
@@ -1,23 +1,26 @@
 import { NextPage } from 'next';
+import Layout from '../../components/layout';
 
 const Chat: NextPage = () => {
   return (
-    <div className='py-10 divide-y-[1px]'>
-      {[...Array(5)].map((_, idx) => (
-        <div
-          key={idx}
-          className='px-4 flex cursor-pointer py-3 items-center space-x-3'
-        >
-          <div className='w-12 aspect-square rounded-full bg-slate-300' />
-          <div>
-            <p className='text-base text-gray-700'>Steve Jebs</p>
-            <p className='text-sm text-gray-500'>
-              See you tomorrow in the corner at 2pm!
-            </p>
+    <Layout title='채팅' hasTabBar>
+      <div className='divide-y-[1px]'>
+        {[...Array(20)].map((_, idx) => (
+          <div
+            key={idx}
+            className='px-4 flex cursor-pointer py-3 items-center space-x-3'
+          >
+            <div className='w-12 aspect-square rounded-full bg-slate-300' />
+            <div>
+              <p className='text-base text-gray-700'>Steve Jebs</p>
+              <p className='text-sm text-gray-500'>
+                See you tomorrow in the corner at 2pm!
+              </p>
+            </div>
           </div>
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
+    </Layout>
   );
 };
 
