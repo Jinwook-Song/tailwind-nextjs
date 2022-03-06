bc9790cc1ba352e402117c1d8a1dcdcfc8c8edeb
jinwook song <wlsdnr129@naver.com>
2022-03-06 13:35:23 +0900

    chats, 컴포턴트 적용

--- pages/chats/[id].tsx
+++ pages/chats/[id].tsx
@@ -1,77 +1,33 @@
 import type { NextPage } from 'next';
+import Layout from '../../components/layout';
+import Message from '../../components/message';
 
 const ChatDetail: NextPage = () => {
   return (
-    <div className='px-4 py-10 space-y-4'>
-      <div className='flex items-start space-x-2'>
-        <div className='w-8 aspect-square rounded-full bg-slate-300' />
-        <div className='w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md'>
-          <p>Hi how much are you selling them for?</p>
-        </div>
-      </div>
-      <div className='flex flex-row-reverse items-start space-x-2 space-x-reverse'>
-        <div className='w-8 aspect-square rounded-full bg-slate-300' />
-        <div className='w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md'>
-          <p>I want ￦20,000</p>
-        </div>
-      </div>
-      <div className='flex items-start space-x-2'>
-        <div className='w-8 aspect-square rounded-full bg-slate-300' />
-        <div className='w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md'>
-          <p>crazy</p>
-        </div>
-      </div>
-      <div className='flex items-start space-x-2'>
-        <div className='w-8 aspect-square rounded-full bg-slate-300' />
-        <div className='w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md'>
-          <p>Hi how much are you selling them for?</p>
-        </div>
-      </div>
-      <div className='flex flex-row-reverse items-start space-x-2 space-x-reverse'>
-        <div className='w-8 aspect-square rounded-full bg-slate-300' />
-        <div className='w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md'>
-          <p>I want ￦20,000</p>
-        </div>
-      </div>
-      <div className='flex items-start space-x-2'>
-        <div className='w-8 aspect-square rounded-full bg-slate-300' />
-        <div className='w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md'>
-          <p>crazy</p>
-        </div>
-      </div>
-      <div className='flex items-start space-x-2'>
-        <div className='w-8 aspect-square rounded-full bg-slate-300' />
-        <div className='w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md'>
-          <p>Hi how much are you selling them for?</p>
-        </div>
-      </div>
-      <div className='flex flex-row-reverse items-start space-x-2 space-x-reverse'>
-        <div className='w-8 aspect-square rounded-full bg-slate-300' />
-        <div className='w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md'>
-          <p>I want ￦20,000</p>
-        </div>
-      </div>
-      <div className='flex items-start space-x-2'>
-        <div className='w-8 aspect-square rounded-full bg-slate-300' />
-        <div className='w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md'>
-          <p>crazy</p>
-        </div>
-      </div>
-
-      <div className='fixed w-full mx-auto max-w-md bottom-2  inset-x-0'>
-        <div className='flex items-center relative'>
-          <input
-            type='text'
-            className='shadow-sm rounded-full w-full border-gray-300 pr-12 focus:ring-orange-500 focus:border-orange-300 focus:outline-none'
-          />
-          <div className='absolute inset-y-0 flex py-1.5 pr-1.5 right-0'>
-            <button className='flex items-center focus:ring-2 focus:ring-orange-500 focus:ring-offset-[3px] bg-orange-500 rounded-full px-3 text-sm text-white hover:bg-orange-600'>
-              &rarr;
-            </button>
+    <Layout canGoBack title='Steve'>
+      <div className='pb-16 px-4'>
+        {[...Array(10)].map((_, idx) => (
+          <div key={idx} className='space-y-4 mb-4'>
+            <Message message='Hi how much are you selling them for?' />
+            <Message message='I want ￦20,000' reversed />
+            <Message message='미쳤어' />
           </div>
-        </div>
+        ))}
+        <form className='fixed py-2 bg-white  bottom-0 inset-x-0'>
+          <div className='flex relative max-w-md items-center  w-full mx-auto'>
+            <input
+              type='text'
+              className='shadow-sm rounded-full w-full border-gray-300 focus:ring-orange-500 focus:outline-none pr-12 focus:border-orange-500'
+            />
+            <div className='absolute inset-y-0 flex py-1.5 pr-1.5 right-0'>
+              <button className='flex focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 items-center bg-orange-500 rounded-full px-3 hover:bg-orange-600 text-sm text-white'>
+                &rarr;
+              </button>
+            </div>
+          </div>
+        </form>
       </div>
-    </div>
+    </Layout>
   );
 };
 
--- pages/chats/index.tsx
+++ pages/chats/index.tsx
@@ -1,4 +1,5 @@
 import { NextPage } from 'next';
+import Link from 'next/link';
 import Layout from '../../components/layout';
 
 const Chat: NextPage = () => {
@@ -6,18 +7,17 @@ const Chat: NextPage = () => {
     <Layout title='채팅' hasTabBar>
       <div className='divide-y-[1px]'>
         {[...Array(20)].map((_, idx) => (
-          <div
-            key={idx}
-            className='px-4 flex cursor-pointer py-3 items-center space-x-3'
-          >
-            <div className='w-12 aspect-square rounded-full bg-slate-300' />
-            <div>
-              <p className='text-base text-gray-700'>Steve Jebs</p>
-              <p className='text-sm text-gray-500'>
-                See you tomorrow in the corner at 2pm!
-              </p>
-            </div>
-          </div>
+          <Link href={`/chats/${idx}`} key={idx}>
+            <a className='px-4 flex cursor-pointer py-3 items-center space-x-3'>
+              <div className='w-12 aspect-square rounded-full bg-slate-300' />
+              <div>
+                <p className='text-base text-gray-700'>Steve Jebs</p>
+                <p className='text-sm text-gray-500'>
+                  See you tomorrow in the corner at 2pm!
+                </p>
+              </div>
+            </a>
+          </Link>
         ))}
       </div>
     </Layout>
